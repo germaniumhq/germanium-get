0822aad0724af1e1ff390bb940297d5e45f0ff7a
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-02-14 23:59:56 +0100

    Updated the build exe script.

--- bin/build-exe.sh
+++ bin/build-exe.sh
@@ -8,5 +8,6 @@ docker run -it \
     --link nexus \
     -e PYPI_URL="http://nexus:8081/repository/pypi-local/pyp" \
     -e PYPI_INDEX_URL="http://nexus:8081/repository/pypi-local/simple" \
+    -e PYINSTALLER_CHOWN="$(id -u):$(id -g)" \
     $DOCKER_IMAGE
 
