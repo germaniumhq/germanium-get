53153c447eb6f3a1e79076609eac81156acf0f7e
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-02-28 23:04:48 +0100

    Use the default cdrx folder chowning.

--- bin/build-exe.sh
+++ bin/build-exe.sh
@@ -8,6 +8,5 @@ docker run -it \
     --link nexus \
     -e PYPI_URL="http://nexus:8081/repository/pypi-local/pyp" \
     -e PYPI_INDEX_URL="http://nexus:8081/repository/pypi-local/simple" \
-    -e PYINSTALLER_CHOWN="$(id -u):$(id -g)" \
     $DOCKER_IMAGE
 
