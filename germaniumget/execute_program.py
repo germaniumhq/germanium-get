41d5d4b94aaacd68ea37ae95e94bce2341cf9298
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-01 22:35:24 +0100

    Don't die if the program is missing.

--- germaniumget/execute_program.py
+++ germaniumget/execute_program.py
@@ -5,10 +5,14 @@ def execute_program(*program):
     Execute a single program. Returns both stderr and stdout combined.
     The program parameter is an array of the command and the parameters.
     """
-    process = subprocess.Popen(program,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+    try:
+        process = subprocess.Popen(program,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
 
-    return stdout + stderr
+        return stdout + stderr
+    except Exception as e:
+        return "Error: %s" % e
+
 
