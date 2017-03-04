a0e837ccde7a59e2b51a2b4367fc11e1552112c9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-04 23:02:54 +0100

    Also use the right selenium.

--- germaniumget/download_urls.py
+++ germaniumget/download_urls.py
@@ -3,7 +3,7 @@ GECKO_DRIVER_DOWNLOAD_URL="https://github.com/mozilla/geckodriver/releases/downl
 CHROME_DRIVER_DOWNLOAD_URL="https://chromedriver.storage.googleapis.com/2.27/chromedriver_win32.zip"
 EDGE_DRIVER_DOWNLOAD_URL="https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe"
 
-SELENIUM_STANDALONE_JAR_URL="http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar"
+SELENIUM_STANDALONE_JAR_URL="http://selenium-release.storage.googleapis.com/3.1/selenium-server-standalone-3.1.0.jar"
 
 JAVA_JRE_URL="http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jre-8u121-windows-i586.exe"
 
