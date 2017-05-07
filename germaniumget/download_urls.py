7b416d3abecda0e34f67ef0e99e0e16612fbf3f4
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-05-07 22:27:12 +0200

    Version 2.0.3 of Germanium.GET, with updated drivers.

--- germaniumget/download_urls.py
+++ germaniumget/download_urls.py
@@ -1,9 +1,10 @@
-IE_DRIVER_DOWNLOAD_URL="http://selenium-release.storage.googleapis.com/3.1/IEDriverServer_Win32_3.1.0.zip"
-GECKO_DRIVER_DOWNLOAD_URL="https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-win64.zip"
-CHROME_DRIVER_DOWNLOAD_URL="https://chromedriver.storage.googleapis.com/2.28/chromedriver_win32.zip"
+IE_DRIVER_DOWNLOAD_URL="http://selenium-release.storage.googleapis.com/3.3/IEDriverServer_Win32_3.3.0.zip"
+GECKO_DRIVER_DOWNLOAD_URL="https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-win64.zip"
+CHROME_DRIVER_DOWNLOAD_URL="https://chromedriver.storage.googleapis.com/2.29/chromedriver_win32.zip"
+
 EDGE_DRIVER_DOWNLOAD_URL="https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe"
 
-SELENIUM_STANDALONE_JAR_URL="http://selenium-release.storage.googleapis.com/3.3/selenium-server-standalone-3.3.1.jar"
+SELENIUM_STANDALONE_JAR_URL="http://selenium-release.storage.googleapis.com/3.2/selenium-server-standalone-3.2.0.jar"
 
 JAVA_JRE_URL="http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jre-8u121-windows-i586.exe"
 
