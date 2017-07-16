a251042a3b3f8ea8b63da57f917424f1d553d875
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-07-17 01:27:32 +0200

    jre-8u131-windows-i586

--- germaniumget/download_urls.py
+++ germaniumget/download_urls.py
@@ -6,5 +6,5 @@ EDGE_DRIVER_DOWNLOAD_URL="https://download.microsoft.com/download/3/2/D/32D3E464
 
 SELENIUM_STANDALONE_JAR_URL="http://selenium-release.storage.googleapis.com/3.2/selenium-server-standalone-3.2.0.jar"
 
-JAVA_JRE_URL="http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jre-8u121-windows-i586.exe"
+JAVA_JRE_URL="http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jre-8u131-windows-i586.exe"
 
